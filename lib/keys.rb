1299fe0c0324c302501e8daf0a42deef008c97d9
hugo valois <hvgaming@yahoo.com>
2018-09-19 23:58:14 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,5 +9,9 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+  groceries.keys
 end
